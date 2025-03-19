9eb785592154372064aafdce84259dadb41eb5d0
saurabh12571257 <davesaurabh59@gmail.com>
2025-03-19 15:15:38 +0530

    deleted files

--- remove_icon_field.sql
+++
@@ -1,2 +0,0 @@
--- Remove the icon field from categories table
-ALTER TABLE categories DROP COLUMN icon; 
--- remove_name_fields.sql
+++
@@ -1,3 +0,0 @@
--- Remove first_name and last_name fields from users table
-ALTER TABLE users DROP COLUMN first_name;
-ALTER TABLE users DROP COLUMN last_name; 
